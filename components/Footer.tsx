a76967f4d1f7fee11a09d586f65a483713809583
Özgür Sağıroğlu <ozgursagiroglu0@gmail.com>
2023-02-18 00:55:42 +0300

    google analytics added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,42 @@
+import Script from 'next/script';
 import Email from './Email';
 
 const Footer = () => {
   return (
-    <div className="font-inconsolata text-sm text-center py-10">
-      <p>This site has been designed by myself but I’m not a designer :(</p>
-      <p>
-        That’s why, when designing this site, I was inspired by{' '}
-        <a
-          href="https://brittanychiang.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="text-tart-orange">
-          Brittany Chiang’s
-        </a>{' '}
-        site.
-      </p>
-      <p>
-        Özgür Sağıroğlu /{' '}
-        <Email name="info" domain="ozgursagiroglu" tld="com" />
-      </p>
-    </div>
+    <>
+      <div className="font-inconsolata text-sm text-center py-10">
+        <p>This site has been designed by myself but I’m not a designer :(</p>
+        <p>
+          That’s why, when designing this site, I was inspired by{' '}
+          <a
+            href="https://brittanychiang.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-tart-orange">
+            Brittany Chiang’s
+          </a>{' '}
+          site.
+        </p>
+        <p>
+          Özgür Sağıroğlu /{' '}
+          <Email name="info" domain="ozgursagiroglu" tld="com" />
+        </p>
+      </div>
+
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-TGJXHV6TB8"
+      />
+      <Script id="analytics">
+        {`
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+   
+     gtag('config', 'G-TGJXHV6TB8');
+    `}
+      </Script>
+    </>
   );
 };
 
