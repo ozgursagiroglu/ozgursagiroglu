6fc37b58e56f3b8f1b450cda1b03aa78ed69c5b3
Özgür Sağıroğlu <info@ozgursagiroglu.com>
2025-07-17 11:48:46 +0400

    Refactor configuration and update dependencies
    - Removed experimental appDir setting from next.config.js.
    - Updated package.json dependencies to latest versions, including major updates for React, Next.js, and TypeScript.
    - Deleted unused head.tsx file and moved its content to layout.tsx for better structure.
    - Added metadata and viewport settings in layout.tsx.
    - Updated yarn.lock to reflect changes in dependencies.
    - Updated public/picture.png with a new version.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
+import { Inconsolata, Inter } from 'next/font/google';
 import '@/styles/globals.css';
-import { Inconsolata, Inter } from '@next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -12,6 +13,20 @@ const inconsolata = Inconsolata({
   display: 'swap',
 });
 
+export const metadata = {
+  title: 'Özgür Sağıroğlu - Senior Full-Stack Developer',
+  description:
+    "I'm a full-stack developer and I consult companies for their projects to develop and optimization.",
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -21,12 +36,23 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${inter.variable} ${inconsolata.variable} antialiased`}>
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body className="bg-cetacean-blue text-alice-blue">{children}</body>
+      <body className="bg-cetacean-blue text-alice-blue">
+        {children}
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TGJXHV6TB8"
+        />
+        <Script id="analytics">
+          {`
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+   
+     gtag('config', 'G-TGJXHV6TB8');
+    `}
+        </Script>
+      </body>
     </html>
   );
 }
